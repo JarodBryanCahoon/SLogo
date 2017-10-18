4ebfd98e717b69573080a228521976a7a3838bf1
Unknown <lasia.lo@duke.edu>
2017-10-18 19:58:40 -0400

    Create ConsoleWindow Class

---
+++ src/frontend/windows/ConsoleWindow.java
@@ -0,0 +1,13 @@
+package frontend.windows;
+
+import javafx.scene.Parent;
+
+public class ConsoleWindow extends Window {
+
+	public ConsoleWindow(int width, int height) {
+		super(width, height, new FlowPane());
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
