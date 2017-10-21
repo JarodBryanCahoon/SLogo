7705f1dd79ecdca143ec9f9fffd330e5373acd18
Jarod Cahoon <jbc53@duke.edu>
2017-10-21 14:06:14 -0400

    Started adding the basics of the turtle class

---
+++ src/boardObject/ObjectInterface.java
@@ -0,0 +1,28 @@
+package boardObject;
+
+/**
+ * 
+ * @author Jarod Cahoon
+ *
+ */
+public interface ObjectInterface{
+	public double moveForward();
+	
+	public double moveBackwards();
+	
+	public double turnLeft();
+	
+	public double turnRight();
+	
+	public double setAngle();
+	
+	public double setPostion();
+	
+	public double setHome();
+	
+	public double clearScreen();
+	
+	public double Hide();
+	
+	public double Show();	
+}
---
+++ src/boardObject/Turtle.java
@@ -0,0 +1,14 @@
+package boardObject;
+
+/**
+ * 
+ * @author Jarod Cahoon
+ *
+ */
+public class Turtle {
+		private double myXPos;
+		private double myYPos;
+		private double myAngle;
+		private boolean myPenDown;
+		
+}
