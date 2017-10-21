672126b0580180d9c35833645c811266772c94c0
Jarod Cahoon <jbc53@duke.edu>
2017-10-21 16:07:30 -0400

    Added more to the turtle class

---
+++ src/backend/board/ConcreteObject.java
@@ -0,0 +1,60 @@
+package backend.board;
+
+public class ConcreteObject implements GeneralObject {
+	
+	public double moveForward() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double moveBackwards() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double turnLeft() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double turnRight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double setAngle() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double setPostion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double setHome() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double clearScreen() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double Hide() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double Show() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double setPostion(double x, double y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/backend/board/TurtleMath.java
@@ -0,0 +1,21 @@
+package backend.board;
+import java.lang.Math;
+/**
+ * 
+ * @author Jarod Cahoon
+ * This class is for static methods which help calculate certain properties for the Turtle class
+ */
+public class TurtleMath {
+	public static double[] xyDeltaCalc(double distance, double angle) {
+		double[] arr = {distance*Math.cos(toDegrees(angle)), distance*Math.sin(toDegrees(angle))};
+		return arr;
+	}
+	
+	public static double toDegrees(double a) {
+		return a*Math.PI/180;
+	}
+	
+	public static double pointDistance(double x1, double y1, double x2, double y2) {
+		return Math.sqrt(Math.pow((x2-x1),2)+ Math.pow((y2-y1), 2));
+	}
+}
