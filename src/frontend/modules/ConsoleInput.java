69bcd65f35c968733288a7e3f5dc60a08d935039
Unknown <lasia.lo@duke.edu>
2017-10-22 17:45:00 -0400

    Multiline console

--- src/frontend/modules/ConsoleInput.java
+++ src/frontend/modules/ConsoleInput.java
@@ -1,10 +1,14 @@
 package frontend.modules;
+import javafx.beans.Observable;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -20,22 +24,34 @@ import javafx.scene.text.TextFlow;
 
 public class ConsoleInput extends Module{
 	private Group myParent;
-	private TextField inputField;
+	private TextArea inputField;
+	private TextArea multiInputField;
 	private TextFlow syntaxField;
 	private TestBackend backend;
+	private KeyCombination keyComb;
 	
 	private int test;
 
 	public ConsoleInput(int width,int height,TestBackend backend){
 		super(width, height);
+		keyComb = new KeyCodeCombination(KeyCode.ENTER,KeyCombination.SHIFT_DOWN);
 		this.backend = backend;
 				
+//		addmultiField();
 		addSyntaxField();
 		addInputField(width);
 		myParent.getChildren().add(inputField);
 		myParent.getChildren().add(syntaxField);
 	}
 	
+	private void addmultiField() {
+		multiInputField = new TextArea();
+		multiInputField.getStyleClass().add("inputField");
+//		multiInputField.setEditable(false);
+		myParent.getChildren().add(multiInputField);
+		
+	}
+
 	@Override
 	protected Parent createParent() {
 		myParent = new Group();
@@ -43,47 +59,45 @@ public class ConsoleInput extends Module{
 	}
 	
 	private void addSyntaxField() {
-		Font.loadFont(
-				  ConsoleInput.class.getResource("/resource/style/Letter Gothic.otf").toExternalForm(), 
-				  10
-				);
+
 		syntaxField = new TextFlow();
-		syntaxField.setLayoutX(10.9);
-		syntaxField.setLayoutY(6);
+		syntaxField.setLayoutX(10);
+		syntaxField.setLayoutY(5);
 		syntaxField.getStyleClass().add("syntaxField");
 	}
 
 	private void addInputField(int width) {
-		
-		inputField = new TextField();
-		inputField.setMinWidth(width);
+		inputField = new TextArea();
+		inputField.setPrefWidth(width);
 		inputField.getStyleClass().add("inputField");
+		inputField.setWrapText(true);
 		inputField.setOnKeyPressed(event -> doSomething(event));
+		inputField.textProperty().addListener(text ->doSomethingElse(text));
 	}
+	
+	private Object doSomethingElse(Observable in) {
+		String text = inputField.textProperty().getValue();
+		createText(text);
+		
+		return null;
+	}
+
 	private void doSomething(KeyEvent event) {
-		String incomingText = inputField.getText();
-		
-		if (event.getCode() == KeyCode.SPACE) {
-			createText(incomingText);
-		}
-		
-		if (event.getCode() == KeyCode.BACK_SPACE && !incomingText.equals("")) {
-			incomingText = incomingText.substring(0,incomingText.length()-1);
-			createText(incomingText);
-		}
-		
+		String text = inputField.textProperty().getValue();
 		if (event.getCode() == KeyCode.ENTER) {
-			backend.addToHistory(incomingText);
+			createText(text);
+		}
+		if (keyComb.match(event)) {
+			backend.addToHistory(text);
 			inputField.setText("");
 			syntaxField.getChildren().clear();
 		}
 	}
 
 	private void createText(String incomingText) {
-		syntaxField.getChildren().clear();
-		Text a = new Text(incomingText.substring(0,incomingText.length()));
-		a.getStyleClass().add("text");
-		syntaxField.getChildren().add(a);
+		myParent.getChildren().clear();
+		myParent.getChildren().add(inputField);
+		myParent.getChildren().addAll(backend.getConsole(incomingText));
 	}
 
 }
--- src/test/ModulesTester.java
+++ src/test/ModulesTester.java
@@ -1,18 +1,23 @@
 package test;
 
+import frontend.modules.ConsoleInput;
 import frontend.modules.ConsoleModule;
 import frontend.modules.Module;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class ModulesTester extends Application{
-	private int WIDTH= 300;
+	private int WIDTH= 500;
 	private int HEIGHT = 500;
 	@Override
 	public void start(Stage stage) throws Exception {
-		
+		Font.loadFont(
+				  ConsoleInput.class.getResource("/resource/style/Letter Gothic.otf").toExternalForm(), 
+				  10
+				);
 		// Change test to whatever module you want
 		Module test = new ConsoleModule (WIDTH,HEIGHT);
 		
