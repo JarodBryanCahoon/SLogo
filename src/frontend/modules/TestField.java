73c9aaec7aa465ed142c84549b4ace4d4959aa67
Unknown <lasia.lo@duke.edu>
2017-10-22 00:27:19 -0400

    Fixed editing

--- src/frontend/modules/TestField.java
+++ src/frontend/modules/TestField.java
@@ -3,6 +3,7 @@ package frontend.modules;
 import java.util.List;
 
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.TextField;
@@ -14,31 +15,43 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 public class TestField{
-	private StackPane inputField;
+	private Group inputField;
 	private TextField inputText;
 	private TextFlow textFlow;
 
 	public TestField(int width, int height){
 		textFlow = new TextFlow();
-		textFlow.setStyle("-fx-background-color : gray;");
-		inputField = new StackPane();
-		inputField.setMinWidth(width);
+		textFlow.setLayoutX(10.8);
+		textFlow.setLayoutY(7.5);
+		textFlow.setStyle("-fx-background-color : black");
+		inputField = new Group();
 		createTextInput();
+			
 	}
 
 	private void createTextInput() {
 		
 		inputText = new TextField();
-		
-		inputText.setStyle("-fx-background-color: transparent;");
+		inputText.setMinWidth(1000);
+		inputText.setStyle("-fx-text-fill: black; -fx-font-size: 16;-fx-background-color: gray;");
 		inputText.setOnKeyPressed(event -> doSomething(event));
 		
-		inputField.getChildren().add(textFlow);
 		inputField.getChildren().add(inputText);
+		inputField.getChildren().add(textFlow);
 	}
 	private void doSomething(KeyEvent event) {
 		textFlow.getChildren().clear();
-		textFlow.getChildren().add(new Text(inputText.getText()));
+		String incomingText = inputText.getText();
+		if (event.getCode() == KeyCode.BACK_SPACE) {
+			incomingText = incomingText.substring(0,incomingText.length()-1);
+			System.out.println(incomingText);
+		}
+		Text a = new Text(incomingText);
+//		Text b = new Text("|");
+		a.setStyle("-fx-fill: red; -fx-font-size: 16;");
+//		b.setStyle("-fx-text-fill: green; -fx-font-size: 16;");
+		textFlow.getChildren().add(a);
+//		textFlow.getChildren().add(b);
 		
 		if (event.getCode() == KeyCode.ENTER)
 			System.out.println("Sending out!");
