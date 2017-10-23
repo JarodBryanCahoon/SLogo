865ec510e750150b6b1d2c5779bd31dbd8d39f08
Unknown <lasia.lo@duke.edu>
2017-10-22 21:25:50 -0400

    Adding variable Modules

---
+++ src/frontend/modules/VariableModule.java
@@ -0,0 +1,18 @@
+package frontend.modules;
+
+import javafx.scene.Parent;
+
+public class VariableModule extends Module{
+	private GridPane myParent;
+	public VariableModule(int width, int height) {
+		super(width, height);
+	}
+
+	@Override
+	protected Parent createParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
