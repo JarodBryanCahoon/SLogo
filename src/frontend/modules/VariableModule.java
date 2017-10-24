27ddb2e96bdd0080f2bdccc5ba1515708dd68749
Unknown <lasia.lo@duke.edu>
2017-10-24 19:02:59 -0400

    Albert fucked things up and I fixed it

---
+++ src/frontend/modules/VariableModule.java
@@ -0,0 +1,20 @@
+package frontend.modules;
+
+import javafx.scene.Parent;
+import javafx.scene.layout.GridPane;
+
+public class VariableModule extends Module{
+	private GridPane myParent;
+	
+	public VariableModule(double width, double height) throws Exception {
+		super(width, height);
+	}
+
+	@Override
+	protected Parent createParent() {
+		
+		return null;
+	}
+	
+
+}
