1fb358d107530a6101ef36022a27108b3c2345eb
Albert Xue <asx@duke.edu>
2017-10-27 14:28:40 -0400

    adding syntax properties file

--- src/backend/interpreter/Word.java
+++ src/backend/interpreter/Word.java
@@ -1,6 +1,10 @@
 package backend.interpreter;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 import backend.abstractSyntaxTree.DoubleExp;
@@ -10,36 +14,61 @@ import backend.abstractSyntaxTree.ListExp;
 import backend.abstractSyntaxTree.MonoOperatorExp;
 import backend.abstractSyntaxTree.NoneOperatorExp;
 import backend.abstractSyntaxTree.VariableExp;
+import exceptions.ErrorMessage;
 
+/**
+ * @author Albert
+ *
+ */
 public class Word {
+	private static final String SYNTAX_PATH = "src/resources/languages/Syntax.properties";
+	private static final String CONSTANT = "Constant";
+	private static final String VARIABLE = "Variable";
+	private static final String COMMAND = "Command";
+	private static final String PREFIX = "backend.board.interfacemovement.";
 	private String myName;
 	private String myType;
 	private Expression myExpression;
 	private int operatorNumber;
+	private Properties myProperties;
+
 //	private Map<String, Integer> commandNumbers = new HashMap<String, Integer>();
 	
 	
 	public Word(String s, ResourceBundle resources, Map<String, Integer> commandNumbers) {	
 		myName = s;
+		addPropertiesFile();
 		determineType(resources, commandNumbers);
 	}
 	
+	private void addPropertiesFile() {
+		myProperties = new Properties();
+		InputStream input = null;
+
+		try {
+
+			input = new FileInputStream(SYNTAX_PATH);
+			myProperties.load(input);
+		} catch (IOException ex) {
+			ErrorMessage eMessage = new ErrorMessage("Could Not Load Properties File");
+		}
+	}
 	
 	private void determineType(ResourceBundle rb, Map<String, Integer> map) {
-		if(myName.matches("^-?[0-9]+.[0-9]+$")){
-			myType = "Constant";
+		if(myName.matches(myProperties.getProperty(CONSTANT))){ 
+			myType = CONSTANT;
 			myExpression = new DoubleExp(Double.parseDouble(myName));
 		}
-		else if(myName.matches("^:[a-zA-Z_]+$")) {
-			myType = "Variable";
+		else if(myName.matches(myProperties.getProperty(VARIABLE))) {
+			myType = VARIABLE;
 			myExpression = new VariableExp(myName);
 		}
-		else if(myName.matches("^[a-zA-Z_]+(\\?)?$")) {
-			myType = "Command";
+		else if(myName.matches(myProperties.getProperty(COMMAND))) {
+			myType = COMMAND;
 			try {
 				String method = rb.getString(myName).split(",")[1];
 				System.out.println(method);
-				method = "backend.board.interfacemovement." + method;
+				method = PREFIX + method;
 				System.out.println(map.entrySet());
 				if (map.get(method)==0) {
 					myExpression = new NoneOperatorExp(method);
---
+++ src/frontend/menus/strategies/ChangeLanguage.java
@@ -0,0 +1,16 @@
+package frontend.menus.strategies;
+
+import frontend.modules.ViewModule;
+
+public class ChangeLanguage extends MenuItemStrategy {
+	public ChangeLanguage(ViewModule view) {
+		super(view);
+	}
+	
+	@Override
+	public void execute() {
+		
+		
+	}
+
+}
