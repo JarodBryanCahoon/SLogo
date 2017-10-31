71f787f008a006f5602c58c768b67faad1ffb1e1
Venkat Subramaniam <venkat.subramaniam@duke.edu>
2017-10-30 20:00:23 -0400

    accidentally working on master--working on for loops

--- src/backend/control/For.java
+++ src/backend/control/For.java
@@ -2,28 +2,33 @@ package backend.control;
 
 import java.util.Map;
 
-public class DoTimes extends ControlNode{	
+public class For extends ControlNode{
+    private double limit = 0;
 	private VariableNode count;
-	private int limit;
-	public DoTimes(Map<String, VariableNode> variables) {
+	private String[] params;
+	private double init;
+	private double increment;
+	
+	public For(Map<String, VariableNode> variables) {
 		super(variables);
 	}
-	
+
 	@Override
 	public double execute() {
 		makeVariable();
-		double result = 0;
-		for (int i=1; i<=limit; i++) {
-			count.setValue(i);
+		double result= 0;
+		for (double i = init ; i<=limit; i+=increment) {
 			result = super.getChildren().get(1).execute();
 		}
 		return result;
 	}
-
+	
 	private void makeVariable() {
 		ListNode myList = (ListNode) super.getChildren().get(0);
-		String[] params = myList.getContents();
-		limit = Integer.parseInt(params[1]);
+		params = myList.getContents();
+		limit = Double.parseDouble(params[2]);
+		increment = Double.parseDouble(params[3]);
+		init = Double.parseDouble(params[1]);
 		if (!super.getVariables().containsKey(params[0])){
 			count = new VariableNode(params[0]);
 			super.getVariables().put(params[0], count);
@@ -31,6 +36,7 @@ public class DoTimes extends ControlNode{
 		else {
 			count = super.getVariables().get(params[0]);
 		}
+		
+		count.setValue(init);
 	}
-
 }
